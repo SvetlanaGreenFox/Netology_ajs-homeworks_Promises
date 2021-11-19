a65bc955abc3be80a8a351b700a3db2de975dfcb
Светлана <svetlanagreenfox@gmail.com>
2021-11-19 14:22:43 +0300

    fixed linter errors

--- src/__tests__/loader.test.js
+++ src/__tests__/loader.test.js
@@ -1,17 +1,11 @@
-
 import GameSavingLoader from '../js/gamesavingloader';
 
-// test('Promis', async () => {
-//   const data = await GameSavingLoader.load();
-//   const expected = '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}';
-//   expect(data).toContain(expected);
-// });
-
-test('Promis', () => {
-  return GameSavingLoader.load().then(data => expect(data).toEqual(
-    {
-      id: 9,
-      created: 1546300800,
-      userInfo: { id: 1, name: 'Hitman', level: 10, points: 2000 }
-    }));
-})
+test('Promis', () => GameSavingLoader.load().then((data) => expect(data).toEqual(
+  {
+    id: 9,
+    created: 1546300800,
+    userInfo: {
+      id: 1, name: 'Hitman', level: 10, points: 2000,
+    },
+  },
+)));
